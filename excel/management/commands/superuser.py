c90dd1ceecda085b046995b4a865832daadbf682
sasashi334 <masashi.sasago@gmail.com>
2025-05-16 00:50:48 +0900

    debug

--- excel/management/commands/superuser.py
+++ excel/management/commands/superuser.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.conf import settings
 
-User = get_user_model
+User = get_user_model()
 
 class Command(BaseCommand):
     def handle(self, *args, **option):
