10593716b14e83f2f753f095f6d71463ee5b8317
sasashi334 <masashi.sasago@gmail.com>
2025-05-16 00:56:50 +0900

    debug

--- excelproject/settings.py
+++ excelproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ex9*460*!d-tmevlzg3(1-vukj85l*&p9ggrfcd8offt0=lkwd"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
