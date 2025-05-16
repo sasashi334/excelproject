8eed2af3b2011abe8d63848f98cb6469d068b9d6
sasashi334 <masashi.sasago@gmail.com>
2025-05-16 16:41:56 +0900

    debug

--- excelproject/urls.py
+++ excelproject/urls.py
@@ -24,7 +24,7 @@ from django.views.static import serve
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),  #追加
+    path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),  #追加
     path("", include("excel.urls")),
 ]
 
