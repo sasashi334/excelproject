8943a197c6c687a5f779c2ed5e74bd09a090c0bd
sasashi334 <masashi.sasago@gmail.com>
2025-05-16 16:27:51 +0900

    debug

--- excelproject/settings.py
+++ excelproject/settings.py
@@ -115,6 +115,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'excel/static/'),
 )
 
+#MEDIA_URL="media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
