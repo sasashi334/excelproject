8c027dbda40417bc89009b4750f26d3ae80e3887
sasashi334 <masashi.sasago@gmail.com>
2025-05-16 15:24:36 +0900

    debug

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit
 pip install -r requirements.txt
-python3 manage.py collectstatic --no-input
+python3 manage.py collectstatic
 python manage.py migrate sessions
 python3 manage.py superuser
