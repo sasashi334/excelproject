3b6d7db4132c8b54783f2e858d9604a403a56ad3
sasashi334 <masashi.sasago@gmail.com>
2026-01-28 03:51:57 +0900

    commit

--- excelproject/settings.py
+++ excelproject/settings.py
@@ -134,7 +134,7 @@ USE_TZ = False
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),  
+        default=os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
     )
 }
